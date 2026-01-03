624c4d3ca89f249a3a1fb8be94021e33660e91b9
Devnm0 <88513019+RobloxExploitDev@users.noreply.github.com>
2026-01-03 19:34:21 +0000

    Create Loadstring.lua

---
+++ Loadstring.lua
@@ -0,0 +1,3 @@
+--Join Discord pls 
+-- https://discord.gg/qVT2yR63fq
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxExploitDev/PIggy-Page-AutoCollector/refs/heads/main/script.lua"))()
